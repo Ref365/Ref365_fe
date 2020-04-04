d3eadbee09c813c4b3eabd6b4320b5e86b18d62e
Noah Gambee <noah@gambee.dev>
2020-04-04 11:40:48 -0600

    Include boilerplaate apollo set up and render emails on NavBar component

--- src/App.svelte
+++ src/App.svelte
@@ -1,11 +1,49 @@
 <script>
+	import NavBar from './components/NavBar.svelte'
+	import About from './components/About.svelte'
 	export let name;
+	import { Router, Link, Route } from "svelte-routing";
+	export let url = '';
+	import ApolloClient from "apollo-boost";
+	import { setClient } from "svelte-apollo";
+	
+	// apollo set up
+
+	const client = new ApolloClient({
+		uri: "http://localhost:3000/graphql",
+    onError: ({ networkError, graphQLErrors }) => {
+      console.log("graphQLErrors", graphQLErrors);
+      console.log("networkError", networkError);
+    }
+	});
+
+  setClient(client);
+
+// standard fetch call 
+
+	// fetch('http://localhost:3000/graphql', {
+	// 	method: 'POST',
+	// 	// mode: 'no-cors',
+	// 	headers: { 'Content-Type': 'application/json' },
+	// 	body: JSON.stringify({ query: '{ users { email } }' }),
+	// })
+	// 	.then(res => {
+	// 		console.log('response:', res)
+	// 		return res.json()
+	// 		})
+	// 	.then(data => console.log('data:', data));
+
 </script>
 
-<main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-</main>
+<Router url={url}>
+	<main>
+		<NavBar />
+		<h1>Hello {name}!</h1>
+		<h2> new change </h2>
+		<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+		<Route path='about' component={About} /> 
+	</main>
+</Router>
 
 <style>
 	main {
