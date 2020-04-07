68babcd12e48622f4863ccd153104a8718d32360
Noah Gambee <noah@gambee.dev>
2020-04-07 13:30:24 -0600

    Write out addEvent mutation and plug in input values

--- src/components/NewEventForm.svelte
+++ src/components/NewEventForm.svelte
@@ -2,25 +2,38 @@
 // imports
   import ApolloClient from "apollo-boost";
   import { gql } from "apollo-boost";
-  import { setClient } from "svelte-apollo";
+  import { setClient, getClient, mutate } from "svelte-apollo";
 // component variables and props
   let newEventDetails = {}
+  let userId = 9;
 // methods
   const submitNewEvent = (e) => {
     e.preventDefault();
     console.log(newEventDetails)
   }
 
-  const ADD_EVENT = gql`
+  const client = getClient();
+  const GETEVENTS = gql`
+    {
+      user(id: ${userId}) {
+        events {
+          date
+        }
+      }
+    }
+  `;
+  // const eventQuery = query(client, {query: GETEVENTS});
+
+  const ADDEVENT = gql`
     mutation {
       createEvent(input: {
-        userId: 7,
-        title: "Soccer",
-        body: "Main ref",
-        date: "2018-07-21 00:00:00 UTC",
-        time: "2020-04-04 14:00:00 UTC",
-        mileage: 13.44,
-        income: 122.33
+        userId: ${userId},
+        title: ${newEventDetails.title},
+        body: ${newEventDetails.notes},
+        date: ${newEventDetails.date},
+        time: ${newEventDetails.time},
+        mileage: ${newEventDetails.mileage},
+        income: ${newEventDetails.payment}
       }) {
         event {
           id
@@ -35,10 +48,27 @@
       }
 }
   `;
+
+  const addEvent = () => {
+   const eventAdd = mutate(client, {
+      mutation: ADDEVENT,
+      // variables: {
+      //   todoEdit
+      // }
+    })
+      .then(data => {
+        // todoEdit = "";
+        // todoOp.refetch();
+        console.log(data)
+      })
+      .catch(e => {
+        console.error("error: ", e);
+      });
+  }
 </script>
 
 
-<form on:submit={submitNewEvent}>
+<form on:submit|preventDefault={addEvent}>
   <label for='event-title'>Event Title</label>
     <input bind:value={newEventDetails.title} class='event-title' type='text'>
   <label for='mileage'>Mileage</label>
@@ -47,13 +77,41 @@
     <input bind:value={newEventDetails.date} class='date' type='date'>
     <input bind:value={newEventDetails.time} type='time' >
   <label for='payment'>Payment</label>
-    <input bind:value={newEventDetails.payment} class='payment' type='number' placeholder='$'>
+    <input bind:value={newEventDetails.payment} placeholder='$' class='payment' type='number' >
   <label for='notes'>Notes</label>
-    <input bind:value={newEventDetails.notes} type='text'>
+    <input class='notes-input' bind:value={newEventDetails.notes} type='text'>
   <button>Create New Event</button> 
 </form>
 
 
 <style>
- 
+  form {
+    padding: 1rem;
+    width: 50%;
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-start;
+  }
+
+  input {
+    border: .1rem solid black;
+    width: 60%;
+    height: 3rem;
+  }
+
+  .notes-input {
+    height: 6rem;
+  }
+  
+  label {
+    width: 60%;
+    font-size: 1.5rem;
+  }
+
+  button {
+    width: 60%;
+    cursor: pointer;
+    border-radius: 5%;
+    background-color: lightcoral
+  }
 </style>
