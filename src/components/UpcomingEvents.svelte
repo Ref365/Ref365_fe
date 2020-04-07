443a8c3b3fd391df73494142acb6d66f8da1aa95
Noah Gambee <noah@gambee.dev>
2020-04-07 14:23:20 -0600

    Add events query to get upcoming events displayed on the page

--- src/components/UpcomingEvents.svelte
+++ src/components/UpcomingEvents.svelte
@@ -1,8 +1,39 @@
 <script>
+  import ApolloClient from "apollo-boost";
+  import { gql } from "apollo-boost";
+  import { setClient, getClient, mutate, query } from "svelte-apollo";
+  let userId = 2;
+
+ const client = getClient();
+ const GETEVENTS = gql`
+    {
+      user(id: ${userId}) {
+        events {
+          date
+          title
+          time
+        }
+      }
+    }
+  `;
+ const eventQuery = query(client, {query: GETEVENTS});
+
 </script>
 
 <section>
   <p>this is the upcoming events section</p>
+   {#await $eventQuery}
+    <p>.. loading</p>
+  {:then data}
+
+    {#each data.data.user.events as event, i}
+      <p>{event.title}</p>
+      <p>{event.date}</p>
+    {/each}
+
+  {:catch e}
+    {e}
+  {/await}
 </section>
 
 <style>
