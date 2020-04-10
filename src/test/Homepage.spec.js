2c425255e54b8da3062927a9d27f22ff3fc2a9a4
Melissa Robbins <melissamrobbins@gmail.com>
2020-04-09 20:02:17 -0600

    Add test for Travis CI

---
+++ src/test/Homepage.spec.js
@@ -0,0 +1,7 @@
+// import '@testing-library/jest-dom/extend-expect';
+// import About from '../components/About';
+
+test('test1', () => {
+  // const { getByText } = render(About)
+  // expect().toBeInTheDocument()
+})
