1b013fca5ecc1b5b806bdb5a43f5037da0c3d075
Melissa Robbins <melissamrobbins@gmail.com>
2020-04-13 16:41:49 -0600

    Add functions to get data to date and previous year's data
    Co-Authored-By: Noah Gambee <noah@gambee.dev>

--- src/components/Dashboard.svelte
+++ src/components/Dashboard.svelte
@@ -11,6 +11,7 @@
         user(id: ${userId}) {
           events {
             mileage
+            dateTime
           }
         }
       }
@@ -21,6 +22,7 @@
         user(id: ${userId}) {
           events {
             income
+            dateTime
           }
         }
       }
@@ -28,6 +30,36 @@
 
   const incomeQuery = query(client, {query: GETALLINCOME});
   const mileageQuery = query(client, {query: GETALLMILEAGE});
+  const getIncomeTotal = events => {
+    let totalIncome = events.reduce((acc, event) => {
+      return acc + event.income;
+    }, 0);
+  };
+  const getDataToDate = events => {
+    let date = new Date();
+    let currentYear = date.getFullYear();
+    let yearBegin = new Date(currentYear, 0, 1).getTime();
+    let eventsToDate = events.filter(event => {
+      let date = new Date(event.dateTime);
+      let milliseconds = date.getTime();
+      return (milliseconds > yearBegin) && (milliseconds < Date.now());
+    });
+    let incomeToDate = eventsToDate.reduce((acc, event) => {
+      return acc + event.income;
+    }, 0);
+  };
+  const getPreviousYearData = events => {
+    let date = new Date();
+    let currentYear = date.getFullYear();
+    let previousYear = currentYear - 1;
+    let prevYearData = events.filter(event => {
+      let eventYear = new Date(event.dateTime).getFullYear();
+      return eventYear === previousYear;
+    });
+    let prevYearIncome = prevYearData.reduce((acc, event) => {
+      return acc + event.income;
+    }, 0);
+  };
 </script>
 
 <section class='dashboard-main'>
@@ -35,7 +67,9 @@
   {#await $incomeQuery}
   <p> ...loading</p>
   {:then data}
-  {console.log('income data array:', data.data.user.events)}
+  {getIncomeTotal(data.data.user.events)}
+  {getDataToDate(data.data.user.events)}
+  {getPreviousYearData(data.data.user.events)}
   {:catch e}
   {/await}
 </section>
