9d4505626a05054a7a9c87623889b03c756cdf97
Noah Gambee <noah@gambee.dev>
2020-04-13 20:08:17 -0600

    Return variables out of each filtering method onto page

--- src/components/Dashboard.svelte
+++ src/components/Dashboard.svelte
@@ -30,11 +30,14 @@
 
   const incomeQuery = query(client, {query: GETALLINCOME});
   const mileageQuery = query(client, {query: GETALLMILEAGE});
+
   const getIncomeTotal = events => {
     let totalIncome = events.reduce((acc, event) => {
       return acc + event.income;
     }, 0);
+    return totalIncome;
   };
+
   const getDataToDate = events => {
     let date = new Date();
     let currentYear = date.getFullYear();
@@ -47,7 +50,9 @@
     let incomeToDate = eventsToDate.reduce((acc, event) => {
       return acc + event.income;
     }, 0);
+    return incomeToDate;
   };
+
   const getPreviousYearData = events => {
     let date = new Date();
     let currentYear = date.getFullYear();
@@ -59,23 +64,38 @@
     let prevYearIncome = prevYearData.reduce((acc, event) => {
       return acc + event.income;
     }, 0);
+    return prevYearIncome;
   };
+
 </script>
 
 <section class='dashboard-main'>
   <h1>Dashboard</h1>
   {#await $incomeQuery}
-  <p> ...loading</p>
+  <p>...loading</p>
   {:then data}
-  {getIncomeTotal(data.data.user.events)}
-  {getDataToDate(data.data.user.events)}
-  {getPreviousYearData(data.data.user.events)}
+  <div class='total-income data-card'>
+    <h2>{getIncomeTotal(data.data.user.events)}</h2>
+  </div>
+  <div class='income-to-date data-card'>
+    <h2>{getDataToDate(data.data.user.events)}</h2>
+  </div>
+  <div class='previous-year-income data-card'>
+    <h2>{getPreviousYearData(data.data.user.events)}</h2>
+  </div>
   {:catch e}
   {/await}
+
 </section>
 
 
 
 <style>
+  * {
+    color: white;
+  }
 
+  .dashboard-main {
+    height: 100vh;
+  }
 </style>
